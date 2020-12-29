baba354c17d952f1ac3e766def9674175082467e
odedgal67 <62900689+odedgal67@users.noreply.github.com>
2020-12-29 18:11:34 +0200

    Added users and courses

---
+++ splnet/Course.java
@@ -0,0 +1,27 @@
+package splnet;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Course {
+    private int courseNumber;
+    private String courseName;
+    private List<Integer> kdamCourseList;
+    private int maxStudents;
+
+    public Course(int courseNumber, String courseName, List<Integer> kdam, int maxStudents )
+    {
+        this.courseNumber=courseNumber;
+        this.courseName = courseName;
+        this.kdamCourseList = new LinkedList<>();
+        for (Integer num : kdam)
+        {
+            this.kdamCourseList.add(num);
+        }
+        this.maxStudents = maxStudents;
+
+    }
+
+
+
+}
